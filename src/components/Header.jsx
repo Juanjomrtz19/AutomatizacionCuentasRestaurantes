4f5bd672bef897ecd0e594e356c0abe5ded4ae76
juanjomrtz19 <jjmartagui@gmail.com>
2024-09-24 22:16:20 +0200

    cambioHome

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ useEffect(() => {
 return(
     <>
     <header className={`bg-[url('./assets/restaurante_movil.jpg')] bg-cover bg-no-repeat flex justify-center h-96 w-full items-center text-gray-100 ${scrollPosition > 384 ? '' : 'flex'} }`}>
-        <h1 className="text-3xl font-bold p-10">Automatiza las comandas de tus clientes</h1>
+        <h1 className="text-3xl font-bold p-10">Automatiza las comandas de tus clientes con <span className="text-purple-700">COMANDADIRECTA</span></h1>
     </header>
     
     {scrollPosition > 384 && (<HeaderFix/>)}
